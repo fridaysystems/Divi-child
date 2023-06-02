ef0ad6075b8dfa01c79e8fb08e675223241ac78c
Corey Salzano <corey.salzano@gmail.com>
2023-06-02 14:08:28 -0400

    Bug fix in navigation, match the actual name I gave to the new API pagination method.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,3 +1,3 @@
 <div class="pagination clearfix">
-	<?php if ( class_exists( 'INVP' ) && is_callable( array( 'INVP', 'paging_html' ) ) ) { echo INVP::paging_html(); } ?>
+	<?php if ( class_exists( 'INVP' ) && is_callable( array( 'INVP', 'get_paging_html' ) ) ) { echo INVP::get_paging_html(); } ?>
 </div>
